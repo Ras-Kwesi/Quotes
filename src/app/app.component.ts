04b6ba2506e11394f7005387766784422697acb8
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-08-10 15:55:37 +0300

    New property added, poster. Output added to component html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import {Quote} from './quote';
+import {Quote} from './quote'; // importing the quote component to parent component
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent {  // Quote object is declared
   quote = [
-  new Quote (0,"",""),
+  new Quote (0,"","",""),
   ]
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,3 @@
 export class Quote {  // Making quote class available to components
-     constructor(public id:number, public quotation:string, public author:string){}
+     constructor(public id:number, public quotation:string, public author:string, public poster:string){}
 }
